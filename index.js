6867d676e3b5d93a6f4cd00827128e33b963ec49
Angelica Hanley <angelicahanley2@gmail.com>
2026-09-21 20:37:01 -0400

    API setup

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Your code here
+var weatherURL = "https://api.openweathermap.org/data/2.5/weather"
+
+
+
+var weatherDiv = document.getElementById('weather-app')
+var form = document.querySelector('form')
+
+form.onsubmit = function(e){
+    e.preventDefault()
+    var searchTerm = this.search.value.trim()
+    if(!searchTerm) return
+    form.search.value=""
+    var queryString = "?units=imperial&appid=97a1ede70f8018568a9fe55a08dff43f&q=" + searchTerm
+    var fetchURL = weatherURL + queryString
+    fetch(fetchURL)
+    .then(function(res){
+        return res.json()
+    })
+    .then(function(weatherData){
+        console.log(weatherData)
+        weatherDiv.removeChild(form)
+    })
+}
